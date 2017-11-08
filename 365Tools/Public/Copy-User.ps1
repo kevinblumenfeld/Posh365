b578183dcc8d8442920bad9fe33abb29b990acd0
kevinblumenfeld <github@thenext.net>
2017-11-08 13:28:31 -0500

    removed 2nd Dyn Parameter

--- 365Tools/Public/Copy-User.ps1
+++ 365Tools/Public/Copy-User.ps1
@@ -82,25 +82,6 @@ Function Copy-User {
         $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParamName_emaildomain, [string], $AttributeCollection)
         $RuntimeParameterDictionary.Add($ParamName_emaildomain, $RuntimeParameter)
 
-        
-        # Set the dynamic parameters' name
-        $ParamName_datastore = 'Datastore'
-        # Create the collection of attributes
-        $AttributeCollection = New-Object System.Collections.ObjectModel.Collection[System.Attribute]
-        # Create and set the parameters' attributes
-        $ParameterAttribute = New-Object System.Management.Automation.ParameterAttribute
-        $ParameterAttribute.Mandatory = $false
-        $ParameterAttribute.Position = 2
-        # Add the attributes to the attributes collection
-        $AttributeCollection.Add($ParameterAttribute)  
-        # Generate and set the ValidateSet 
-        $arrSet = "1"
-        $ValidateSetAttribute = New-Object System.Management.Automation.ValidateSetAttribute($arrSet)
-        # Add the ValidateSet to the attributes collection
-        $AttributeCollection.Add($ValidateSetAttribute)
-        # Create and return the dynamic parameter
-        $RuntimeParameter = New-Object System.Management.Automation.RuntimeDefinedParameter($ParamName_datastore, [string], $AttributeCollection)
-        $RuntimeParameterDictionary.Add($ParamName_datastore, $RuntimeParameter)
         return $RuntimeParameterDictionary
 
 
