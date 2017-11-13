89c627be6b04add7a5283717391a371629aafccb
kevinblumenfeld <github@thenext.net>
2017-11-13 15:01:55 -0500

    removed debug writes

--- 365Tools/Private/Watch-ToLicense.ps1
+++ 365Tools/Private/Watch-ToLicense.ps1
@@ -25,14 +25,10 @@ Function Watch-ToLicense {
                     Remove-Item $_.VersionInfo.filename -verbose
                 }
                     if ($_.name -eq "ALLDONE") {
-                        WRITE-HOST "EXITONZERO"
                         $ExitOnZero = $True
                         Remove-Item $_.VersionInfo.filename -verbose
                     }
                     if ((Get-ChildItem -Path $GuidFolder).count -lt 1 -and $ExitOnZero) {
-                        WRITE-HOST "SHOULD EXIT HERE"
-                        Stop-Job WatchToLicense
-                        Remove-Job WatchToLicense
                         Exit
                     }
             }
