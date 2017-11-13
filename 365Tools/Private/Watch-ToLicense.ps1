0de5c54c45205d174e00b909d18c7e6b2c7ad5b4
kevinblumenfeld <github@thenext.net>
2017-11-13 17:24:00 -0500

    Remove-Item -Path $GuidFolder -Confirm:$False -force

--- 365Tools/Private/Watch-ToLicense.ps1
+++ 365Tools/Private/Watch-ToLicense.ps1
@@ -30,6 +30,7 @@ Function Watch-ToLicense {
                         Remove-Item $_.VersionInfo.filename -verbose
                     }
                     if ((Get-ChildItem -Path $GuidFolder).count -lt 1 -and $ExitOnZero) {
+                        Remove-Item -Path $GuidFolder -Confirm:$False -force
                         Exit
                     }
             }
