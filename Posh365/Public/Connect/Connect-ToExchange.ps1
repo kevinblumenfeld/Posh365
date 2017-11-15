403f729d5df471146ffc78dd011364710ca8757b
kevinblumenfeld <github@thenext.net>
2017-11-15 11:07:06 -0500

    Import-Module (Import-PSSession -AllowClobber -Force) -Global -DisableNameChecking -Prefix "OnPrem" | Out-Null

--- Posh365/Public/Connect/Connect-ToExchange.ps1
+++ Posh365/Public/Connect/Connect-ToExchange.ps1
@@ -77,7 +77,7 @@ function Connect-ToExchange {
         $Credential.UserName | Out-File ($KeyPath + "$($user).uExchangeCred")
     }    
     $Session = New-PSSession -Name "OnPremExchange" -ConfigurationName Microsoft.Exchange -ConnectionUri ("http://" + $ExchangeServer + "/PowerShell/") -Authentication Kerberos -Credential $Credential
-    Import-Module (Import-PSSession $Session -WarningAction SilentlyContinue) -Global -Prefix "OnPrem" | Out-Null
+    Import-Module (Import-PSSession -AllowClobber -Force) -Global -DisableNameChecking -Prefix "OnPrem" | Out-Null
     Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
     Write-Host "        You are now connected to On-Premises Exchange               " -foregroundcolor "darkgreen" -backgroundcolor "white"
     Write-Host "          All commands are pre-pended with OnPrem, for example:     " -foregroundcolor "darkgreen" -backgroundcolor "white"
