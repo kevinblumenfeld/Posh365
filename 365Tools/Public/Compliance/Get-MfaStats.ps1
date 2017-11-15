df6b4180bd048b25af9d5401895ac3a62d39e323
kevinblumenfeld <github@thenext.net>
2017-11-14 20:40:28 -0500

    tweaked Get-MFA-Stats and help MAML

--- 365Tools/Public/Compliance/Get-MfaStats.ps1
+++ 365Tools/Public/Compliance/Get-MfaStats.ps1
@@ -1,49 +1,49 @@
 function Get-MfaStats {
-    
-        [CmdletBinding()]
-        Param
-        (
-            [Parameter(Mandatory = $true,
-                ValueFromPipeline = $true,
-                ValueFromPipelineByPropertyName = $true)]
-            [string[]] $userprincipalname,
+
+    [CmdletBinding()]
+    Param
+    (
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [string[]] $userprincipalname,
             
-            [Parameter(Mandatory = $false)]
-            [switch] $Archive,
+        [Parameter(Mandatory = $false)]
+        [switch] $Archive,
       
-            [Parameter(Mandatory = $false)]
-            [switch] $StartMFA
-        )
-        Begin {
-            $resultarray = @()
+        [Parameter(Mandatory = $false)]
+        [switch] $StartMFA
+    )
+    Begin {
+        $resultarray = @()
 
     
-        }
-        Process {
-            foreach ($CurUPN in $userprincipalname) {
-                if ($StartMFA) {
-                    Write-Output "Starting Managed Folder Assistant on: $($CurUPN)"
-                    Start-ManagedFolderAssistant $CurUPN
+    }
+    Process {
+        foreach ($CurUPN in $userprincipalname) {
+            if ($StartMFA) {
+                Write-Output "Starting Managed Folder Assistant on: $($CurUPN)"
+                Start-ManagedFolderAssistant $CurUPN
+            }
+            else {
+                if ($Archive) {
+                    $logProps = Export-MailboxDiagnosticLogs $CurUPN -ExtendedProperties -Archive
                 }
                 else {
-                    if ($Archive) {
-                        $logProps = Export-MailboxDiagnosticLogs $CurUPN -ExtendedProperties -Archive
-                    }
-                    else {
-                        $logProps = Export-MailboxDiagnosticLogs $CurUPN -ExtendedProperties
-                    }
-                    $xmlprops = [xml]($logProps.MailboxLog)
-                    $stats = $xmlprops.Properties.MailboxTable.Property | ? {$_.Name -like "ELC*"} 
-                    $statHash = [ordered]@{}
-                    for ($i = 0; $i -lt $stats.count; $i++) {
-                        $statHash['UPN'] = $CurUPN
-                        $statHash[$stats[$i].name] = $stats[$i].value
-                    }
-                    $resultarray += [PSCustomObject]$statHash
+                    $logProps = Export-MailboxDiagnosticLogs $CurUPN -ExtendedProperties
                 }
+                $xmlprops = [xml]($logProps.MailboxLog)
+                $stats = $xmlprops.Properties.MailboxTable.Property | ? {$_.Name -like "ELC*"} 
+                $statHash = [ordered]@{}
+                for ($i = 0; $i -lt $stats.count; $i++) {
+                    $statHash['UPN'] = $CurUPN
+                    $statHash[$stats[$i].name] = $stats[$i].value
+                }
+                $resultarray += [PSCustomObject]$statHash
             }
         }
-        End {
-            $resultarray
-        }
     }
+    End {
+        $resultarray
+    }
+}
