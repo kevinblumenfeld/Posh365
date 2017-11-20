ce2fc98557438e890ac46bb000dffeed3ee39b4d
kevinblumenfeld <github@thenext.net>
2017-11-20 18:41:08 -0500

    added new functionality in deprovisioning

---
+++ Posh365/Public/User/Add-FullAccessToMailbox.ps1
@@ -0,0 +1,43 @@
+Function Add-FullAccessToMailbox {
+    <#
+    .SYNOPSIS
+    Adds fullaccess mailbox permissions for one or more users
+
+    .EXAMPLE
+    "fred.smith@contoso.com","frank.jones@contoso.com" | Add-FullAccessToMailbox -Mailbox "john.smith@contoso.com"
+   
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+        [string] $Mailbox,
+        [Parameter(Mandatory = $false, ValueFromPipeline, ValueFromPipelineByPropertyName)]
+        [string] $UserNeedingAccess
+    )
+    
+    Begin {
+
+        $RootPath = $env:USERPROFILE + "\ps\"
+        $User = $env:USERNAME
+    
+        While (!(Get-Content ($RootPath + "$($user).DomainController") -ErrorAction SilentlyContinue | ? {$_.count -gt 0})) {
+            Select-DomainController
+        }
+        $DomainController = Get-Content ($RootPath + "$($user).DomainController")   
+
+        try {
+            (Get-CloudAcceptedDomain -erroraction stop)[0] | Out-Null
+        }
+        catch {
+            Connect-ToCloud ($targetAddressSuffix = Get-Content ($RootPath + "$($user).TargetAddressSuffix")) -ExchangeOnline -EXOPrefix
+        }
+
+    }
+    Process {
+        
+        Add-CloudMailboxPermission -AccessRights FullAccess -Identity $Mailbox -User $UserNeedingAccess
+    }
+    End {
+    
+    }
+}    
---
+++ Posh365/Public/User/Disable-Employee.ps1
@@ -0,0 +1,56 @@
+Function Disable-Employee {
+    <#
+    .SYNOPSIS
+    Disables the AD User & Removes any licenses.  Also coverts mailbox to a Shared Mailbox when requested
+
+    .EXAMPLE
+    Disable-Employee -UserToDisable JSMITH
+
+    .EXAMPLE
+    Disable-Employee -UserToDisable JSMITH@CONTOSO.COM
+   
+    #>
+    [CmdletBinding()]
+    Param (
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+        [string] $UserToDisable,
+
+        [Parameter(Mandatory = $false, ValueFromPipelineByPropertyName)]
+        [string[]] $UsersToGiveFullAccess
+    )
+    
+    Begin {
+
+        $RootPath = $env:USERPROFILE + "\ps\"
+        $User = $env:USERNAME
+    
+        While (!(Get-Content ($RootPath + "$($user).DomainController") -ErrorAction SilentlyContinue | ? {$_.count -gt 0})) {
+            Select-DomainController
+        }
+        $DomainController = Get-Content ($RootPath + "$($user).DomainController")   
+    }
+    Process {
+        # Convert Cloud Mailbox to type, Shared.
+
+        Convert-ToShared -UserToConvert $UserToDisable
+
+        # Hide from Address List
+        if ($UserToDisable -like "*@*") {
+            Get-ADUser -LDAPFilter "(Userprincipalname=$UserToDisable)" -Server $domainController | 
+                Set-ADUser -replace @{
+                msExchHideFromAddressLists = $True
+            }
+        }
+        else {
+            Get-ADUser -LDAPFilter "(samaccountname=$UserToDisable)" -erroraction stop -Server $domainController | 
+                Set-ADUser -replace @{
+                msExchHideFromAddressLists = $True
+            }
+        }
+        $UsersToGiveFullAccess | Add-FullAccessToMailbox -Mailbox $UserToDisable
+    }
+    
+    End {
+    
+    }
+}
