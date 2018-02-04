87736f851b6abd6cae19a8f935b0a845038576be
kevinblumenfeld <github@thenext.net>
2018-02-04 13:22:40 -0500

    fix switch for SpecifyRetentionPolicy from string

--- Posh365/Public/User/New-UserToCloud.ps1
+++ Posh365/Public/User/New-UserToCloud.ps1
@@ -17,7 +17,7 @@ Function New-UserToCloud {
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = "Copy")]
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = "New")]
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = "Shared")]
-        [string] $SpecifyRetentionPolicy,
+        [switch] $SpecifyRetentionPolicy,
         [Parameter(Mandatory, ValueFromPipelineByPropertyName, ParameterSetName = "Shared")]
         [ValidateScript( {if ($_ -notlike "* *") {Return $True} else {Write-Host "Please choose an SharedMailboxEmailAlias without spaces"}})]
         [string] $SharedMailboxEmailAlias,
