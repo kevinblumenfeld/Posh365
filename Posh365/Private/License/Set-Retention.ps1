096a219cc296bfce2787fd9034cd1009215276cf
kevinblumenfeld <github@thenext.net>
2018-02-04 22:28:56 -0500

    Set-Retention fix argumentlist

--- Posh365/Private/License/Set-Retention.ps1
+++ Posh365/Private/License/Set-Retention.ps1
@@ -23,11 +23,12 @@ Function Set-Retention {
     $RetentionJob = Start-Job -Name Set-Retention {
         $UserPrincipalName = $args[0]
         $RententionPolicyToAdd = $args[1]
+        $targetAddressSuffix = $args[2]
         Connect-Cloud $targetAddressSuffix -ExchangeOnline -EXOPrefix
         while (!(Get-Mailbox $UserPrincipalName -ErrorAction SilentlyContinue)) {
             Start-Sleep -Seconds 30
         }
         Set-CloudMailbox $UserPrincipalName -RetentionPolicy $RetentionPolicyToAdd
         Get-PSSession | Remove-PSSession
-    } -ArgumentList $UserPrincipalName, $RetentionPolicyToAdd | Out-Null 
+    } -ArgumentList $UserPrincipalName, $RetentionPolicyToAdd, $targetAddressSuffix | Out-Null 
 }    
