fb594dbcadfda455c95b4bdbdb0cc6b8a5f03e2b
kevinblumenfeld <github@thenext.net>
2018-02-04 20:40:44 -0500

    Corrected another Braces issue

--- Posh365/Public/Connect/Connect-Exchange.ps1
+++ Posh365/Public/Connect/Connect-Exchange.ps1
@@ -91,30 +91,31 @@ function Connect-Exchange {
             Write-Host "********************************************************************" -foregroundcolor "darkblue" -backgroundcolor "white"
             Break
         }
-        If (!$NoPrefix) {
-            $SessionModule = Import-PSSession -AllowClobber -DisableNameChecking -Prefix 'OnPrem' -Session $Session
-            $Null = Import-Module $SessionModule -Global -Prefix "OnPrem" -DisableNameChecking -Force
-            if ($ViewEntireForest) {
-                Set-OnPremADServerSettings -ViewEntireForest:$True
-            }
-            Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "        You are now connected to On-Premises Exchange               " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "          All commands are pre-pended with OnPrem, for example:     " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "               Get-Mailbox       is      Get-OnPremMailbox          " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host " This is to prevent overlap of commands between Office 365 and EXO  " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "   For example, Get-Mailbox would be used for Office 365 while,     " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "     Get-OnPremMailbox would be used for On-Premises Exchange       " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
-        }
-        else {
-            $SessionModule = Import-PSSession -AllowClobber -DisableNameChecking -Session $Session
-            $Null = Import-Module $SessionModule -Global -DisableNameChecking -Force
-            if ($ViewEntireForest) {
-                Set-ADServerSettings -ViewEntireForest:$True
-            }
-            Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "        You are now connected to On-Premises Exchange               " -foregroundcolor "darkgreen" -backgroundcolor "white"
-            Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
-        }
     }
+    If (!$NoPrefix) {
+        $SessionModule = Import-PSSession -AllowClobber -DisableNameChecking -Prefix 'OnPrem' -Session $Session
+        $Null = Import-Module $SessionModule -Global -Prefix "OnPrem" -DisableNameChecking -Force
+        if ($ViewEntireForest) {
+            Set-OnPremADServerSettings -ViewEntireForest:$True
+        }
+        Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "        You are now connected to On-Premises Exchange               " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "          All commands are pre-pended with OnPrem, for example:     " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "               Get-Mailbox       is      Get-OnPremMailbox          " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host " This is to prevent overlap of commands between Office 365 and EXO  " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "   For example, Get-Mailbox would be used for Office 365 while,     " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "     Get-OnPremMailbox would be used for On-Premises Exchange       " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
+    }
+    else {
+        $SessionModule = Import-PSSession -AllowClobber -DisableNameChecking -Session $Session
+        $Null = Import-Module $SessionModule -Global -DisableNameChecking -Force
+        if ($ViewEntireForest) {
+            Set-ADServerSettings -ViewEntireForest:$True
+        }
+        Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "        You are now connected to On-Premises Exchange               " -foregroundcolor "darkgreen" -backgroundcolor "white"
+        Write-Host "********************************************************************" -foregroundcolor "darkgreen" -backgroundcolor "white"
+    }
+}
     
