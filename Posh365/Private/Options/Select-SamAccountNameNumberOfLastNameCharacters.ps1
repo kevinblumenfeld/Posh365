ea01c44eb10ccd69385df392b86c4b3574b6790b
kevinblumenfeld <github@thenext.net>
2018-02-11 15:38:52 -0500

    removed -PassThru

--- Posh365/Private/Options/Select-SamAccountNameNumberOfLastNameCharacters.ps1
+++ Posh365/Private/Options/Select-SamAccountNameNumberOfLastNameCharacters.ps1
@@ -20,12 +20,12 @@ function Select-SamAccountNameNumberOfLastNameCharacters {
 
     if ($SamAccountNameNumberOfFirstNameCharacters) {
         [array]$SamAccountNameNumberOfLastNameCharacters = 1..($SamAccountNameCharacters - $SamAccountNameNumberOfFirstNameCharacters)  | % {$_ -join ","}  | 
-            Out-GridView -OutputMode Single -PassThru -Title "Select the Maximum number of characters from the user's Last Name that will make up the SamAccountName (Choose 1 and click OK)"    
+            Out-GridView -OutputMode Single -Title "Select the Maximum number of characters from the user's Last Name that will make up the SamAccountName (Choose 1 and click OK)"    
         $SamAccountNameNumberOfLastNameCharacters | Out-File ($RootPath + "$($user).SamAccountNameNumberOfLastNameCharacters") -Force
     }
     else {
         [array]$SamAccountNameNumberOfLastNameCharacters = 1..($SamAccountNameCharacters)  | % {$_ -join ","}  | 
-            Out-GridView Out-GridView -OutputMode Single-PassThru -Title "Select the Maximum number of characters from the user's Last Name that will make up the SamAccountName (Choose 1 and click OK)"  
+            Out-GridView Out-GridView -OutputMode Single -Title "Select the Maximum number of characters from the user's Last Name that will make up the SamAccountName (Choose 1 and click OK)"  
         $SamAccountNameNumberOfLastNameCharacters | Out-File ($RootPath + "$($user).SamAccountNameNumberOfLastNameCharacters") -Force
     }
     
