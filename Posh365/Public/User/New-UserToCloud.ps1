fb8b813d3feb792b8eb204dbc963c97deb046c51
kevinblumenfeld <github@thenext.net>
2018-02-11 22:57:37 -0500

    add start-sleep to delete guid folder watcher function

--- Posh365/Public/User/New-UserToCloud.ps1
+++ Posh365/Public/User/New-UserToCloud.ps1
@@ -494,6 +494,7 @@ Function New-UserToCloud {
                 $GuidFolder = $args[0]
                 New-Item -Path $GuidFolder -Name "ALLDONE" -Type File
                 while ((Get-ChildItem -Path $GuidFolder).count -gt 0) {
+                    Start-Sleep -Seconds 5
                     Write-Host "DELETE GUID WHILE LOOP!!!!!!!!!"
                 }
                 Remove-Item -Path $GuidFolder -Confirm:$False -force -verbose
