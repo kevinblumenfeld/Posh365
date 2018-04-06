700ccb3679e2c7787e6d6ac28ec6737b04b1647f
Kevin Blumenfeld <github@thenext.net>
2018-04-06 13:06:40 -0400

    added new example to use -filter

--- Posh365/Public/Group/Get-EXOGroup.ps1
+++ Posh365/Public/Group/Get-EXOGroup.ps1
@@ -13,6 +13,9 @@ function Get-EXOGroup {
     Get-EXOGroup | Export-Csv c:\scripts\All365GroupExport.csv -notypeinformation -encoding UTF8
     
     .EXAMPLE
+    Get-DistributionGroup -Filter "emailaddresses -like '*contoso.com*'" | Select -ExpandProperty Name | Get-EXOGroup | Export-Csv c:\scripts\365GroupExport.csv -notypeinformation -encoding UTF8
+    
+    .EXAMPLE
     Get-Content "c:\scripts\groups.txt" | Get-EXOGroup | Export-Csv c:\scripts\365GroupExport.csv -notypeinformation -encoding UTF8
     
     Example of groups.txt
