cb4ec74cc251100efa7857507eef7fe42324bf4d
Kevin Blumenfeld <github@thenext.net>
2018-05-22 16:05:05 -0400

    New Functions Get-EXOMigrationStatistics and Get-EXOMoveRequestStatistics

---
+++ Posh365/Public/Migration/Get-EXOMigrationStatistics.ps1
@@ -0,0 +1,53 @@
+Function Get-EXOMigrationStatistics {
+    <#
+    .SYNOPSIS
+    Provides each user found in Get-MigrationUser in an Out-GridView.  The user can select one or more users for the report provided by Get-MigrationUserStatistics -Include report
+    
+    .DESCRIPTION
+    Provides each user found in Get-MigrationUser in an Out-GridView.  The user can select one or more users for the report provided by Get-MigrationUserStatistics -Include report.
+    Each report will open in a seperate Out-GridView
+    
+    .EXAMPLE
+    Get-EXOMigrationStatistics
+
+    #>
+    [CmdletBinding()]
+    param
+    (
+
+    )
+
+    $currentErrorActionPrefs = $ErrorActionPreference
+    $ErrorActionPreference = 'Stop'
+
+    $MigrationUser = Get-MigrationUser -ResultSize Unlimited
+    $MigrationUserDetails = foreach ($User in $MigrationUser) {
+        [PSCustomObject]@{
+            Identity            = $User.Identity
+            MailboxEmailAddress = $User.MailboxEmailAddress
+            SkippedItemCount    = $User.SkippedItemCount
+            SyncedItemCount     = $User.SyncedItemCount
+            BatchId             = $User.BatchId
+            RecipientType       = $User.RecipientType
+            State               = $User.State
+            Status              = $User.Status
+            StatusSummary       = $User.StatusSummary
+            TriggeredAction     = $User.TriggeredAction
+            WorkflowStage       = $User.WorkflowStage
+            WorkflowStep        = $User.WorkflowStep
+            Guid                = $User.Guid
+        }
+    }
+    $WantsDetailOnTheseMigrationUsers = $MigrationUserDetails | Out-GridView -PassThru -Title "Migration Users - Choose one or more and click OK for details"
+    if ($WantsDetailOnTheseMigrationUsers) {
+        Foreach ($Wants in $WantsDetailOnTheseMigrationUsers) {
+            $UserStats = Get-MigrationUserStatistics -Identity $Wants.Guid -IncludeReport
+            $UserStats.Report.Entries | Select-Object CreationTime, @{n = 'Migration User Statistics Report'; e = {$_.message}} |
+                Out-GridView -Title "ID: $($Wants.Identity) EMAIL: $($Wants.MailboxEmailAddress) STATUS: $($Wants.Status) SYNCED: $($Wants.SyncedItemCount) SKIPPED: $($Wants.SkippedItemCount)" 
+        }
+    }
+    else {
+        Write-Verbose "`nNo Results found."
+    }
+    $ErrorActionPreference = $currentErrorActionPrefs
+}
---
+++ Posh365/Public/Migration/Get-EXOMoveRequestStatistics.ps1
@@ -0,0 +1,55 @@
+Function Get-EXOMoveRequestStatistics {
+    <#
+    .SYNOPSIS
+    Provides each user found in Get-MoveRequest in an Out-GridView.  The user can select one or more users for the report provided by Get-MoveRequestStatistics -Include report
+    
+    .DESCRIPTION
+    Provides each user found in Get-MoveRequest in an Out-GridView.  The user can select one or more users for the report provided by Get-MoveRequestStatistics -Include report.
+    Each report will open in a seperate Out-GridView
+    
+    .EXAMPLE
+    Get-EXOMoveRequestStatistics
+
+    #>
+    [CmdletBinding()]
+    param
+    (
+
+    )
+
+    $currentErrorActionPrefs = $ErrorActionPreference
+    $ErrorActionPreference = 'Stop'
+
+    $MoveRequest = Get-MoveRequest -ResultSize Unlimited
+    $MoveRequestDetails = foreach ($Move in $MoveRequest) {
+        [PSCustomObject]@{
+            Identity                   = $Move.Identity
+            Status                     = $Move.Status
+            DisplayName                = $Move.DisplayName
+            Alias                      = $Move.Alias
+            BatchName                  = $Move.BatchName
+            Suspend                    = $Move.Suspend
+            SuspendWhenReadyToComplete = $Move.SuspendWhenReadyToComplete
+            RecipientType              = $Move.RecipientType
+            RecipientTypeDetails       = $Move.RecipientTypeDetails
+            RemoteHostName             = $Move.RemoteHostName
+            RequestStyle               = $Move.RequestStyle
+            TargetDatabase             = $Move.TargetDatabase
+            ExchangeGuid               = $Move.ExchangeGuid
+            Guid                       = $Move.Guid
+            Name                       = $Move.Name
+        }
+    }
+    $WantsDetailOnTheseMoveRequests = $MoveRequestDetails | Out-GridView -PassThru -Title "Move Requests - Choose one or more and click OK for details"
+    if ($WantsDetailOnTheseMoveRequests) {
+        Foreach ($Wants in $WantsDetailOnTheseMoveRequests) {
+            $MoveStats = Get-MoveRequestStatistics -Identity $Wants.Guid -IncludeReport
+            $MoveStats.Report.Entries | Select-Object CreationTime, @{n = 'Move Request Statistics Report'; e = {$_.message}} |
+                Out-GridView -Title "Name: $($Wants.DisplayName) STATUS: $($Wants.Status) BATCH: $($Wants.BatchName)" 
+        }
+    }
+    else {
+        Write-Verbose "`nNo Results found."
+    }
+    $ErrorActionPreference = $currentErrorActionPrefs
+}
