481e89e84dc7da807d9fa73b12c9493e1b0f34bd
Kevin Blumenfeld <github@thenext.net>
2018-07-06 09:33:04 -0400

    added CBH

--- Posh365/Public/OPP/Remove-OfficeLicense.ps1
+++ Posh365/Public/OPP/Remove-OfficeLicense.ps1
@@ -2,27 +2,35 @@ function Remove-OfficeLicense {
     <#
 
 .SYNOPSIS
+Remove Product Key and License from an existing Office Install
+
+.DESCRIPTION
+Remove Product Key and License from an existing Office Install
 Often used when moving from one Office 365 tenant to another.
 
+.EXAMPLE
+Remove-OfficeLicense
+
 #>
+    Start-Transcript
 
-    # store the license info into an array (uncomment line below)
-    $license = cscript "C:\Program Files (x86)\Microsoft Office\Office15\OSPP.VBS" /dstatus
+    $licrem = join-path $env:temp OfficeProPlusLicenseRemoved.txt
+    if (-not (Test-Path $licrem)) {
 
-    #license name from /dstatus
-    $o365 = “ADJUST_HERE_OfficeO365ProPlusR_Subscription1 edition”
+        $license = cscript “C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS” /dstatus
 
-    #loop till the end of the array searching for the $o365 string
-    for ($i = 0; $i -lt $license.Length; $i++) {
+        $o365 = 'LICENSE NAME'
 
-        if ($license[$i] -match $o365) {
+        for ($i = 0; $i -lt $license.Length; $i++) {
 
-            $i += 6 #jumping six lines to get to the product key line in the array, check output of dstatus and adjust as needed for the product you are removing
-            $keyline = $license[$i] # extra step but i would rather deal with the variable as a string than an array, could be removed i guess, efficiency is not my concern
-            $prodkey = $keyline.substring($keyline.length - 5, 5) # getting the last 5 characters of the line (prodkey)
-
+            if ($license[$i] -match $o365) {
+                $i += 6
+                $keyline = $license[$i]
+                $prodkey = $keyline.substring($keyline.length - 5, 5)
+            }
         }
+
+        cscript “C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS” /unpkey:$prodkey
+        New-Item $licrem
     }
-    #removing the key from the workstation
-    cscript ‘C:\Program Files (x86)\Microsoft Office\Office15\OSPP.VBS’ /unpkey:$prodkey
 }
