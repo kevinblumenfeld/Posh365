36e1b2cc5ce929a4ad374e1901d6893861730034
Kevin Blumenfeld <github@thenext.net>
2018-10-04 19:20:16 -0400

    add perm chain gitignore

---
+++ Posh365/Private/Permissions/Get-MigrationChain.ps1
@@ -0,0 +1,41 @@
+﻿function Get-MigrationChain {
+    param (
+        [Parameter(Mandatory = $true,
+            ValueFromPipeline = $true,
+            ValueFromPipelineByPropertyName = $true)]
+        [string[]] $Names,   
+
+        [PSObject[]] $DataSet,
+        
+        [Parameter(Mandatory = $false)]
+        [string] $swvar = $swvar,
+        
+        [Hashtable] $LoopDetection = @{}
+    )
+	
+    if ($swvar -notmatch "FullAccess|SendAs|SendOnBehalf") {
+        $swvar = 'placeholder'
+    }
+    $nameAndDependencies = @()
+    foreach ($Name in $Names) {
+        $DataSet | Where-Object {$_.UPN -eq $Name -and $_.Permission -notmatch $swvar} | ForEach-Object {
+            if ($_.GrantedUPN -and !$LoopDetection.Contains($_.GrantedUPN)) {
+                $LoopDetection.Add($_.GrantedUPN, $null)
+                $nameAndDependencies += Get-MigrationChain -Name $_.GrantedUPN -DataSet $DataSet -LoopDetection $LoopDetection -swvar $swvar
+            }
+        }
+        $DataSet | Where-Object {$_.GrantedUPN -eq $Name -and $_.Permission -notmatch $swvar} | ForEach-Object {
+            if ($_.UPN -and !$LoopDetection.Contains($_.UPN)) {
+                $LoopDetection.Add($_.UPN, $null)
+                $nameAndDependencies += Get-MigrationChain -Name $_.UPN -DataSet $DataSet -LoopDetection $LoopDetection -swvar $swvar
+            }
+        }
+    }
+    # We add the source name only if they are not found in the dependencies
+    foreach ($Name in $Names) {
+        if ($name -notin $nameAndDependencies) {
+            $nameAndDependencies += $Name
+        }
+    }
+    $nameAndDependencies
+}
---
+++ Posh365/Public/Permissions/Get-PermissionChain.ps1
@@ -0,0 +1,86 @@
+﻿function Get-PermissionChain {
+    [CmdletBinding()]
+    <#
+    .SYNOPSIS
+    With the exception of Full Access permissions, mailbox permissions do not currently allow access cross-premises (between Office 365, Exchange OnPrem & vice-versa).
+    Therefore this script will analyze a list your provide to determine which other mailboxes need to be migrated along with your list.
+    
+    Provide a list of UPNs to be migrated to Office 365 and this script will output all mailboxes that need to be migrated with them - Based on mailbox permissions.
+    The output will be a single column of UPNs that will include the original list provided by you.  
+    This can be used as the migration batch file - just add a header of EmailAddress and you are ready to migrate!
+    
+    .EXAMPLE 
+    Get-Content .\UPNs.txt  | Get-PermissionChain | Export-Csv .\MigrationBatch.csv -notypeinformation 
+    
+    .EXAMPLE
+    "mailbox01@contoso.com" | Get-PermissionChain | Export-Csv .\MigrationBatch.csv -notypeinformation
+    
+    .EXAMPLE
+    Get-Content .\UPNs.txt  | Get-PermissionChain -IgnoreFullAccess | Export-Csv .\MigrationBatch.csv -notypeinformation 
+
+    .EXAMPLE
+    Get-Content .\UPNs.txt  | Get-PermissionChain -IgnoreFullAccess -IgnoreSendOnBehalf | Export-Csv .\MigrationBatch.csv -notypeinformation 
+
+    .EXAMPLE
+    "mailbox01@contoso.com","mailbox02@contoso.com" | Get-PermissionChain | Export-Csv .\MigrationBatch.csv -notypeinformation
+    
+    #>
+    param (
+        [Parameter(Mandatory = $false, ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        [string] $Names,
+
+        [Parameter(Mandatory = $true)]
+        [System.IO.FileInfo] $ReportPath,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $IgnoreFullAccess,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $IgnoreSendAs,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $IgnoreSendOnBehalf,
+
+        [Parameter(Mandatory = $false)]
+        [switch] $DontCombineOutputwithOriginalList,
+
+        [Parameter(Mandatory = $false, ParameterSetName = "Cache")]
+        [switch] $GeneratePermissionCacheOnly
+    )
+    Begin {
+        $swvar = @()
+        if ($IgnoreFullAccess) {
+            $swvar += "FullAccess"
+        }
+        if ($IgnoreSendAs) {
+            $swvar += "SendAs"            
+        }
+        if ($IgnoreSendOnBehalf) {
+            $swvar += "SendOnBehalf"            
+        }
+        $swvar = $swvar -join ("|")
+
+        New-Item -ItemType Directory c:\pScripts -ErrorAction SilentlyContinue
+        
+        if (![System.IO.File]::Exists("$ReportPath" + "\allPermissions.csv")) {
+            Get-MailboxPerms -ReportPath "C:\PermsReports"
+        }
+        If ($GeneratePermissionCacheOnly) {
+            Break
+        }
+                
+        $Data = Import-Csv ("$ReportPath" + "allPermissions.csv")
+    }
+    Process {
+        
+        $Output += ($Names | Get-MigrationChain -DataSet $Data -swvar $swvar)
+
+    }
+    End {
+        # Add Original List to Results
+        Foreach ($name in $names) {
+            $Output += $name
+        }
+        $Output | Sort -Unique
+    }
+}
