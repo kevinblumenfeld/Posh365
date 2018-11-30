d635c355a5a5ecb77d02ee1eccf70cb0588f74fd
Kevin Blumenfeld <github@thenext.net>
2018-11-30 10:33:54 -0500

    corrected PreFlighttemplate

--- Posh365/Public/Migration/New-PreFlightTemplate.ps1
+++ Posh365/Public/Migration/New-PreFlightTemplate.ps1
@@ -5,6 +5,13 @@ function New-PreFlightTemplate {
         [string] $FileNamePath
     )
     
-    $Header = "Check,MailboxType,RecipientType,BatchName,RemoteHostName,UserPrincipalName,RetentionPolicy,ForwardingAddress,ForwardingSmtpAddress,DeliverToMailboxAndForward,samAccountName,ActiveSyncEnabled,IsSynchronized,CloudUPN,UPNsMatch,DisplayName,PrimarySMTP,UPNSMTPMatch,IsLicensed,GoodAddresses,RoutingAddress,FlipUPNPassword,Migrating,MailboxMigrated,ActiveSyncEnabledPostMigration,ForwardingPostMigration,PreflightComplete,MoveRequest,MoveStatus,MoveBatch,ErrorCloud,ErrorOnPrem"
-    $Header | Export-Csv $FileNamePath -NoTypeInformation -Encoding UTF8
+    if (-not (Test-Path $FileNamePath)) {
+
+        $Header = "Check,MailboxType,RecipientType,BatchName,RemoteHostName,UserPrincipalName,RetentionPolicy,ForwardingAddress,ForwardingSmtpAddress,DeliverToMailboxAndForward,samAccountName,ActiveSyncEnabled,IsSynchronized,CloudUPN,UPNsMatch,DisplayName,PrimarySMTP,UPNSMTPMatch,IsLicensed,GoodAddresses,RoutingAddress,FlipUPNPassword,Migrating,MailboxMigrated,ActiveSyncEnabledPostMigration,ForwardingPostMigration,PreflightComplete,MoveRequest,MoveStatus,MoveBatch,ErrorCloud,ErrorOnPrem"
+        Set-Content -Path $FileNamePath -Value $Header
+
+    }
+    else {
+        Write-Warning "File already exists.  Please choose another file name and try again."
+    }
 }
