c9e6f240281f7193a91b963ed6e436f10057a192
Kevin Blumenfeld <github@thenext.net>
2018-12-06 18:45:50 -0500

    Updated Get-AdReplication to account for LDAP Server Down

--- Posh365/Public/Discovery/Get-ADReplication.ps1
+++ Posh365/Public/Discovery/Get-ADReplication.ps1
@@ -2,15 +2,15 @@ function Get-ADReplication {
     Param ()
     <#
     .SYNOPSIS
-    Get Active Directory Replication Summary from each Domain Controller in the Forest 
-    
+    Get Active Directory Replication Summary from each Domain Controller in the Forest
+
     .EXAMPLE
-    Get-ADReplicaton | Export-Csv .\ADReplication.csv -notypeinformation
+    Get-ADReplication | Export-Csv .\ADReplication.csv -notypeinformation
 
     #>
 
     $domain = (Get-ADForest).Domains
-    
+
     $server = ForEach ($curDomain in $domain) {
         Try {
             Get-ADDomainController -Discover -DomainName $curDomain -erroraction stop
@@ -31,69 +31,110 @@ function Get-ADReplication {
 
     foreach ($curDC in $dc) {
         $src = @(& C:\windows\system32\repadmin.exe /replsummary /bysrc /sort:delta $curDC.hostname)
+
         $cleanSrc = @()
         for ($i = 10; $i -lt ($src.Count - 4); $i++) {
             if ($src[$i] -ne "") {
-                $src[$i] -replace '\s+', ' ' | Out-Null           
-                $cleanSrc += $src[$i]           
+                $src[$i] -replace '\s+', ' ' | Out-Null
+                $cleanSrc += $src[$i]
             }
         }
+
         foreach ($line in $cleanSrc) {
-            if ($line -like "* days*") {
-                $line = ($line -replace ' days', 'days')
-            }
-            $splitSrc = $line -split '\s+', 8
-            if ($splitSrc[0] -eq "Source") {
-                $repType = "Source" 
-            }
-            if ($splitSrc[0] -eq "Destination") {
-                $repType = "Destination"
-            }
-            if ($splitSrc[1] -notmatch "DSA") {
+            if ($line -like "*Server Down*") {
+                $ErrorMsg = ""
+                for ($i = 0; $i -lt ($cleanSrc.count - 1); $i++) {
+                    $ErrorMsg += $cleanSrc[$i]
+                }
                 New-Object PSCustomObject -Property @{
                     Testing  = $curDC.hostname
                     ADSite   = $curDC.Site
-                    DSAType  = $repType
-                    Hostname = $splitSrc[1]
-                    Delta    = $splitSrc[2]
-                    Fails    = $splitSrc[3]
-                    Total    = $splitSrc[5]
-                    PctError = $splitSrc[6]   
-                    ErrorMsg = $splitSrc[7]
+                    DSAType  = "Source"
+                    Hostname = ""
+                    Delta    = ""
+                    Fails    = ""
+                    Total    = ""
+                    PctError = ""
+                    ErrorMsg = $ErrorMsg.trim()
                 } | Select-Object Testing, ADSite, DSAType, Hostname, Delta, Fails, Total, PctError, ErrorMsg
             }
-        } 
+            else {
+                if ($line -like "* days*") {
+                    $line = ($line -replace ' days', 'days')
+                }
+                $splitSrc = $line -split '\s+', 8
+                if ($splitSrc[0] -eq "Source") {
+                    $repType = "Source"
+                }
+                if ($splitSrc[0] -eq "Destination") {
+                    $repType = "Destination"
+                }
+                if ($splitSrc[1] -notmatch "DSA") {
+                    New-Object PSCustomObject -Property @{
+                        Testing  = $curDC.hostname
+                        ADSite   = $curDC.Site
+                        DSAType  = $repType
+                        Hostname = $splitSrc[1]
+                        Delta    = $splitSrc[2]
+                        Fails    = $splitSrc[3]
+                        Total    = $splitSrc[5]
+                        PctError = $splitSrc[6]
+                        ErrorMsg = $splitSrc[7]
+                    } | Select-Object Testing, ADSite, DSAType, Hostname, Delta, Fails, Total, PctError, ErrorMsg
+                }
+            }
+
+        }
         $dest = @(& C:\windows\system32\repadmin.exe /replsummary /bydst /sort:delta $curDC.hostname)
         $cleandest = @()
         for ($i = 10; $i -lt ($dest.Count - 4); $i++) {
             if ($dest[$i] -ne "") {
-                $dest[$i] -replace '\s+', ' ' | Out-Null           
-                $cleandest += $dest[$i]           
+                $dest[$i] -replace '\s+', ' ' | Out-Null
+                $cleandest += $dest[$i]
             }
         }
         foreach ($line in $cleandest) {
-            if ($line -like "* days*") {
-                $line = ($line -replace ' days', 'days')
-            }
-            $splitDest = $line -split '\s+', 8
-            if ($splitDest[0] -eq "Source") {
-                $repType = "Source" 
-            }
-            if ($splitDest[0] -eq "Destination") {
-                $repType = "Destination"
-            }
-            if ($splitDest[1] -notmatch "DSA") {
+            if ($line -like "*Server Down*") {
+                $ErrorMsg = ""
+                for ($i = 0; $i -lt ($cleanSrc.count - 1); $i++) {
+                    $ErrorMsg += $cleanSrc[$i]
+                }
                 New-Object PSCustomObject -Property @{
                     Testing  = $curDC.hostname
                     ADSite   = $curDC.Site
-                    DSAType  = $repType
-                    Hostname = $splitDest[1]
-                    Delta    = $splitDest[2]
-                    Fails    = $splitDest[3]
-                    Total    = $splitDest[5]
-                    PctError = $splitDest[6]   
-                    ErrorMsg = $splitDest[7]
+                    DSAType  = "Destination"
+                    Hostname = ""
+                    Delta    = ""
+                    Fails    = ""
+                    Total    = ""
+                    PctError = ""
+                    ErrorMsg = $ErrorMsg.trim()
                 } | Select-Object Testing, ADSite, DSAType, Hostname, Delta, Fails, Total, PctError, ErrorMsg
+            }
+            else {
+                if ($line -like "* days*") {
+                    $line = ($line -replace ' days', 'days')
+                }
+                $splitDest = $line -split '\s+', 8
+                if ($splitDest[0] -eq "Source") {
+                    $repType = "Source"
+                }
+                if ($splitDest[0] -eq "Destination") {
+                    $repType = "Destination"
+                }
+                if ($splitDest[1] -notmatch "DSA") {
+                    New-Object PSCustomObject -Property @{
+                        Testing  = $curDC.hostname
+                        ADSite   = $curDC.Site
+                        DSAType  = $repType
+                        Hostname = $splitDest[1]
+                        Delta    = $splitDest[2]
+                        Fails    = $splitDest[3]
+                        Total    = $splitDest[5]
+                        PctError = $splitDest[6]
+                        ErrorMsg = $splitDest[7]
+                    } | Select-Object Testing, ADSite, DSAType, Hostname, Delta, Fails, Total, PctError, ErrorMsg
+                }
             }
         }
     }
