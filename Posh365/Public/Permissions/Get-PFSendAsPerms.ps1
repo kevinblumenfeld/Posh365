0367c1967e355893bae2162b74e7219e79983a45
Kevin Blumenfeld <github@thenext.net>
2019-01-31 15:37:02 -0500

    new PF perms testing now

---
+++ Posh365/Public/Permissions/Get-PFSendAsPerms.ps1
@@ -0,0 +1,87 @@
+﻿function Get-PFSendAsPerms {
+    <#
+    .SYNOPSIS
+    Outputs Send As permissions for each mailbox that has permissions assigned.
+    This is for On-Premises Exchange 2010, 2013, 2016+
+
+    .EXAMPLE
+
+    (Get-MailPublicFolder -ResultSize unlimited | Select -expandproperty distinguishedname) | Get-PFSendAsPerms | Export-csv .\PFSA.csv -NoTypeInformation
+
+    If not running from Exchange Management Shell (EMS), run this first:
+
+    Connect-Exchange -NoPrefix
+
+    #>
+    [CmdletBinding()]
+    Param (
+        [parameter(ValueFromPipeline = $true, ValueFromPipelineByPropertyName = $true)]
+        $DistinguishedName,
+
+        [parameter()]
+        [hashtable] $ADHashDN,
+
+        [parameter()]
+        [hashtable] $ADHash
+    )
+    Begin {
+        Try {
+            import-module activedirectory -ErrorAction Stop -Verbose:$false
+        }
+        Catch {
+            Write-Host "This module depends on the ActiveDirectory module."
+            Write-Host "Please download and install from https://www.microsoft.com/en-us/download/details.aspx?id=45520"
+            throw
+        }
+    }
+    Process {
+        ForEach ($curDN in $DistinguishedName) {
+            $mailbox = $curDN
+            Write-Verbose "Inspecting: `t $mailbox"
+            Get-ADPermission $curDN | Where-Object {
+                $_.ExtendedRights -like "*Send-As*" -and
+                ($_.IsInherited -eq $false) -and
+                !($_.User -like "NT AUTHORITY\SELF") -and
+                !($_.User.tostring().startswith('S-1-5-21-')) -and
+                !$_.Deny
+            } | ForEach-Object {
+                $User = $_.User
+                Write-Verbose "Has Send As: `t $User"
+                try {
+                    Get-ADGroupMember ($_.user -split "\\")[1] -Recursive -ErrorAction stop |
+                        ForEach-Object {
+                        New-Object -TypeName psobject -property @{
+                            Object             = $ADHashDN["$mailbox"].DisplayName
+                            UPN                = $ADHashDN["$mailbox"].UPN
+                            PrimarySMTPAddress = $ADHashDN["$mailbox"].PrimarySMTPAddress
+                            Granted            = $ADHashDN["$($_.distinguishedname)"].DisplayName
+                            GrantedUPN         = $ADHashDN["$($_.distinguishedname)"].UPN
+                            GrantedSMTP        = $ADHashDN["$($_.distinguishedname)"].PrimarySMTPAddress
+                            Checking           = $User
+                            GroupMember        = $($_.distinguishedname)
+                            Type               = "GroupMember"
+                            Permission         = "SendAs"
+                        }
+                    }
+                }
+                Catch {
+                    New-Object -TypeName psobject -property @{
+                        Object             = $ADHashDN["$mailbox"].DisplayName
+                        UPN                = $ADHashDN["$mailbox"].UPN
+                        PrimarySMTPAddress = $ADHashDN["$mailbox"].PrimarySMTPAddress
+                        Granted            = $ADHash["$User"].DisplayName
+                        GrantedUPN         = $ADHash["$User"].UPN
+                        GrantedSMTP        = $ADHash["$User"].PrimarySMTPAddress
+                        Checking           = $User
+                        GroupMember        = ""
+                        Type               = "User"
+                        Permission         = "SendAs"
+                    }
+                }
+            }
+        }
+    }
+    END {
+
+    }
+}
---
+++ Posh365/Public/Permissions/Get-PFSendOnBehalfPerms.ps1
@@ -0,0 +1,84 @@
+function Get-PFSendOnBehalfPerms {
+    <#
+    .SYNOPSIS
+    Outputs SendOnBehalf permissions for each mailbox that has permissions assigned.
+    This is for On-Premises Exchange 2010, 2013, 2016+
+
+    .EXAMPLE
+
+	(Get-Mailbox -ResultSize unlimited | Select -expandproperty distinguishedname) | Get-PFSendOnBehalfPerms | Export-csv .\SOB.csv -NoTypeInformation
+
+    If not running from Exchange Management Shell (EMS), run this first:
+
+    Connect-Exchange -NoPrefix
+
+    #>
+    [CmdletBinding()]
+    Param (
+        [parameter(ValueFromPipeline = $true)]
+        $DistinguishedName,
+
+        [parameter()]
+        [hashtable] $ADHashDN,
+
+        [parameter()]
+        [hashtable] $ADHashCN
+    )
+    Begin {
+        Try {
+            import-module activedirectory -ErrorAction Stop -Verbose:$false
+        }
+        Catch {
+            Write-Host "This module depends on the ActiveDirectory module."
+            Write-Host "Please download and install from https://www.microsoft.com/en-us/download/details.aspx?id=45520"
+            throw
+        }
+    }
+    Process {
+        ForEach ($curDN in $DistinguishedName) {
+            $mailbox = $curDN
+            Write-Verbose "Inspecting: `t $Mailbox"
+            (Get-MailPublicFolder $curDN -erroraction silentlycontinue).GrantSendOnBehalfTo |
+                where-object {$_ -ne $null}  | ForEach-Object {
+                $CN = $_
+                $DisplayName = $ADHashCN["$CN"].DisplayName
+                Write-Verbose "Has Send On Behalf DN: `t $DisplayName"
+                Write-Verbose "                   CN: `t $CN"
+                try {
+                    Get-ADGroupMember "$DisplayName" -Recursive -ErrorAction stop |
+                        ForEach-Object {
+                        New-Object -TypeName psobject -property @{
+                            Object             = $ADHashDN["$mailbox"].DisplayName
+                            UPN                = $ADHashDN["$mailbox"].UPN
+                            PrimarySMTPAddress = $ADHashDN["$mailbox"].PrimarySMTPAddress
+                            Granted            = $ADHashDN["$($_.distinguishedname)"].DisplayName
+                            GrantedUPN         = $ADHashDN["$($_.distinguishedname)"].UPN
+                            GrantedSMTP        = $ADHashDN["$($_.distinguishedname)"].PrimarySMTPAddress
+                            Checking           = $CN
+                            GroupMember        = $($_.distinguishedname)
+                            Type               = "GroupMember"
+                            Permission         = "SendOnBehalf"
+                        }
+                    }
+                }
+                Catch {
+                    New-Object -TypeName psobject -property @{
+                        Object             = $ADHashDN["$mailbox"].DisplayName
+                        UPN                = $ADHashDN["$mailbox"].UPN
+                        PrimarySMTPAddress = $ADHashDN["$mailbox"].PrimarySMTPAddress
+                        Granted            = $ADHashCN["$CN"].DisplayName
+                        GrantedUPN         = $ADHashCN["$CN"].UPN
+                        GrantedSMTP        = $ADHashCN["$CN"].PrimarySMTPAddress
+                        Checking           = $CN
+                        GroupMember        = ""
+                        Type               = "User"
+                        Permission         = "SendOnBehalf"
+                    }
+                }
+            }
+        }
+    }
+    END {
+
+    }
+}
