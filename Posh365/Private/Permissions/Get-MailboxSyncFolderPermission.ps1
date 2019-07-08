0b9cd802fbc7de626280bb34b12bc0d3f28e492b
Kevin Blumenfeld <github@thenext.net>
2019-07-08 11:02:53 -0400

    Get-MailboxSyncFolderPermission called wrong hash

--- Posh365/Private/Permissions/Get-MailboxSyncFolderPermission.ps1
+++ Posh365/Private/Permissions/Get-MailboxSyncFolderPermission.ps1
@@ -11,7 +11,7 @@
     )
 
     Write-Verbose "Caching hashtable. DisplayName as Key and Values of UPN, PrimarySMTP, msExchRecipientTypeDetails & msExchRecipientDisplayType"
-    $ADHashDisplayName = $MailboxList | Get-ADHashCN
+    $ADHashDisplayName = $MailboxList | Get-ADHashDisplayName
 
     Write-Verbose "Getting Folder Permissions for each mailbox and writing to file"
     $MailboxList | Get-MailboxFolderPerms -ADHashDisplayName $ADHashDisplayName | Select-Object $FolderSelect
