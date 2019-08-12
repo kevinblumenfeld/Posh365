1603e0ee0583295bf665beea7089a7aabbedcc94
Kevin Blumenfeld <github@thenext.net>
2019-08-11 20:32:35 -0400

    Completed Update-MailboxMoveBatchesReport

--- Posh365/Public/Migration/MailboxMove/PostMigrationTask/Update-MailboxMoveBatchesReport.ps1
+++ Posh365/Public/Migration/MailboxMove/PostMigrationTask/Update-MailboxMoveBatchesReport.ps1
@@ -18,14 +18,14 @@ function Update-MailboxMoveBatchesReport {
 
     .PARAMETER NewCsvFile
     Path to csv of mailboxes. Minimum headers required are: BatchName, UserPrincipalName
-    This would be a new Csv of existing mailboxes from source environment
+    This would be a new Csv of existing mailboxes that you want to update with BatchNames from the current excel on the SharePoint Team Site
 
     .PARAMETER Tenant
     This is the tenant domain - where you are migrating to.
-    Example if tenant is contoso.mail.onmicrosoft.com use contoso
+    Example if tenant is contoso.mail.onmicrosoft.com use: Contoso
 
     .EXAMPLE
-    Update-MailboxMoveBatchesReport -SharePointURL 'https://fabrikam.sharepoint.com/sites/Contoso' -ExcelFile 'Batches.xlsx' -Tenant Contoso
+    Update-MailboxMoveBatchesReport -SharePointURL https://fabrikam.sharepoint.com/sites/Contoso -ExcelFile batches.xlsx -Tenant Contoso -NewCsvFile C:\Scripts\Batches.csv -ReportPath C:\Scripts\
 
     .NOTES
     General notes
@@ -51,30 +51,94 @@ function Update-MailboxMoveBatchesReport {
         [Parameter(Mandatory)]
         [ValidateNotNullOrEmpty()]
         [string]
-        $NewCsvFile
+        $NewCsvFile,
+
+        [Parameter(Mandatory)]
+        [string]
+        $ReportPath
     )
     end {
         if ($Tenant -notmatch '.mail.onmicrosoft.com') {
             $Tenant = '{0}.mail.onmicrosoft.com' -f $Tenant
         }
+        New-Item -ItemType Directory -Path $ReportPath -ErrorAction SilentlyContinue
         $SharePointSplat = @{
             SharePointURL = $SharePointURL
             ExcelFile     = $ExcelFile
             Tenant        = $Tenant
         }
         $CurrentHash = @{ }
-        $CurrentList = (Import-SharePointExcel @SharePointSplat).where{ $_.BatchName }
+        $CurrentList = (Import-SharePointExcel @SharePointSplat).where{ $_.BatchName -or $_.IsMigrated -or $_.CompleteBatchDate -or $_.CompleteBatchTimePT }
         foreach ($Current in $CurrentList) {
-            $CurrentHash.Add($Current.UserPrincipalName, $Current.BatchName)
+            $CurrentHash.Add($Current.UserPrincipalName, @{
+                    'BatchName'           = $Current.BatchName
+                    'IsMigrated'          = $Current.IsMigrated
+                    'CompleteBatchDate'   = $Current.CompleteBatchDate
+                    'CompleteBatchTimePT' = $Current.CompleteBatchTimePT
+                }
+            )
         }
-        $FutureList = Import-Csv $NewCsvFile
-        $SelectProps = ($FutureList[0].psobject.properties.name).where{ $_ -ne 'BatchName' }
-        $FutureList | Select-Object @(
-            $SelectProps
+        # $SelectProps = ($FutureList[0].psobject.properties.name).where{ $_ -notmatch 'BatchName|IsMigrated|CompleteBatchDate|CompleteBatchTimePT' }
+        $Future = Import-Csv $NewCsvFile | Select-Object @(
             @{
                 Name       = 'BatchName'
-                Expression = { $CurrentHash.$($_.UserPrincipalName) }
+                Expression = { $CurrentHash.$($_.UserPrincipalName).BatchName }
             }
+            'DisplayName'
+            'OrganizationalUnit'
+            @{
+                Name       = 'IsMigrated'
+                Expression = { $CurrentHash.$($_.UserPrincipalName).IsMigrated }
+            }
+            @{
+                Name       = 'CompleteBatchDate'
+                Expression = { $CurrentHash.$($_.UserPrincipalName).CompleteBatchDate }
+            }
+            @{
+                Name       = 'CompleteBatchTimePT'
+                Expression = { $CurrentHash.$($_.UserPrincipalName).CompleteBatchTimePT }
+            }
+            'MailboxGB'
+            'ArchiveGB'
+            'DeletedGB'
+            'TotalGB'
+            'LastLogonTime'
+            'ItemCount'
+            'UserPrincipalName'
+            'PrimarySmtpAddress'
+            'AddressBookPolicy'
+            'RetentionPolicy'
+            'AccountDisabled'
+            'Alias'
+            'Database'
+            'OU'
+            'Office'
+            'RecipientTypeDetails'
+            'UMEnabled'
+            'ForwardingAddress'
+            'ForwardingRecipientType'
+            'ForwardingSmtpAddress'
+            'DeliverToMailboxAndForward'
         )
+        $ExcelSplat = @{
+            Path                    = (Join-Path $ReportPath 'Batches.xlsx')
+            TableStyle              = 'Medium2'
+            FreezeTopRowFirstColumn = $true
+            AutoSize                = $true
+            BoldTopRow              = $true
+            ClearSheet              = $true
+            WorksheetName           = 'Batches'
+            ErrorAction             = 'SilentlyContinue'
+        }
+        $Future | Sort-Object @(
+            @{
+                Expression = "BatchName"
+                Descending = $true
+            }
+            @{
+                Expression = "DisplayName"
+                Descending = $false
+            }
+        ) | Export-Excel @ExcelSplat
     }
 }
