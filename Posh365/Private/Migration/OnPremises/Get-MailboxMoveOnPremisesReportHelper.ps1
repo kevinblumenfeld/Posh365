94739dd7d9124e53ca76cad62358079233423856
Kevin Blumenfeld <github@thenext.net>
2019-11-28 13:16:26 -0500

    attribute adds for MMOnPremisesReportHelper

--- Posh365/Private/Migration/OnPremises/Get-MailboxMoveOnPremisesReportHelper.ps1
+++ Posh365/Private/Migration/OnPremises/Get-MailboxMoveOnPremisesReportHelper.ps1
@@ -8,14 +8,17 @@
         $MailboxList = Get-Mailbox -ResultSize Unlimited -IgnoreDefaultScope
         foreach ($Mailbox in $MailboxList) {
             Write-Verbose "Mailbox`t$($Mailbox.DisplayName)"
-            $Statistic = $Mailbox | Get-OnPremExchangeMailboxStatistics
+            $Statistic = $Mailbox | Get-ExchangeMailboxStatistics
             $PSHash = @{
                 BatchName                  = ''
                 DisplayName                = $Mailbox.DisplayName
                 OrganizationalUnit         = $Mailbox.OrganizationalUnit
                 IsMigrated                 = ''
-                CompleteBatchOn            = ''
-                CompleteBatchonTimePT      = ''
+                CompleteBatchDate          = ''
+                CompleteBatchTimePT        = ''
+                LicenseGroup               = ''
+                EnableArchive              = ''
+                ConvertToShared            = ''
                 MailboxGB                  = $Statistic.MailboxGB
                 ArchiveGB                  = $Statistic.ArchiveGB
                 DeletedGB                  = $Statistic.DeletedGB
@@ -26,15 +29,15 @@
                 PrimarySmtpAddress         = $Mailbox.PrimarySmtpAddress
                 AddressBookPolicy          = $Mailbox.AddressBookPolicy
                 RetentionPolicy            = $Mailbox.RetentionPolicy
-                AccountDisabled            = $Mailbox.AccountDisabled
+                AccountDisabled            = $Mailbox.UserAccountControl
                 Alias                      = $Mailbox.Alias
                 Database                   = $Mailbox.Database
                 OU                         = ($Mailbox.DistinguishedName -replace '^.+?,(?=(OU|CN)=)')
                 Office                     = $Mailbox.Office
                 RecipientTypeDetails       = $Mailbox.RecipientTypeDetails
                 UMEnabled                  = $Mailbox.UMEnabled
-                DeliverToMailboxAndForward = $Mailbox.DeliverToMailboxAndForward
                 ForwardingSmtpAddress      = $Mailbox.ForwardingSmtpAddress
+                DeliverToMailboxAndForward = $Mailbox.DeliverToMailboxAndForward
             }
             if ($Mailbox.ForwardingAddress) {
                 $Distinguished = Convert-CanonicalToDistinguished -CanonicalName $Mailbox.ForwardingAddress
