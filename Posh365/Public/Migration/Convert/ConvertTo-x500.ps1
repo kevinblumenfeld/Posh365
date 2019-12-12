40010755c57c78ef2cc3f67503dfd7d409de8d84
Kevin Blumenfeld <github@thenext.net>
2019-12-12 11:47:49 -0500

    Correct ConvertTo-x500

--- Posh365/Public/Migration/Convert/ConvertTo-x500.ps1
+++ Posh365/Public/Migration/Convert/ConvertTo-x500.ps1
@@ -20,9 +20,7 @@ function ConvertTo-x500 {
     ConvertTo-x500 -IMCEAEX "IMCEAEX-_o=ExchangeLabs_ou=Exchange+20Administrative+20Group+20+28FYDIBOHF23SPDLT+29_cn=Recipients_cn=86595dbec932d461fbdfe93cb1234585e-Joe+20Smit@namprd13.prod.outlook.com"
 
     .NOTES
-    Thank you to Matt Ellis for this code.
-    Please check out some of his awesome blog posts here:
-    https://medium.com/@MattEllisUC
+    This is based on Matt Ellis's code.  It was missing ("+2C", ",") so I corrected it here.
     #>
 
     param (
@@ -34,12 +32,12 @@ function ConvertTo-x500 {
         Write-Host -ForegroundColor Red "`nSorry, your IMCEAEX string must begin with IMCEAEX`n"
     }
     else {
-        $X500 = $IMCEAEX.Replace("IMCEAEX-", "X500:").Replace("_", "/").Replace("+20", " ").Replace("+28", "(").Replace("+29", ")").Replace("+2E", ".").Replace("%3D", "=").Split("@")[0]
+        $X500 = $IMCEAEX.Replace("IMCEAEX-", "X500:").Replace("_", "/").Replace("+20", " ").Replace("+28", "(").Replace("+29", ")").Replace("+2E", ".").Replace("%3D", "=").Replace("+2C", ",").Split("@")[0]
         Write-Host
         Write-Host -ForegroundColor DarkCyan "Your converted X.500 address is: `n"
         Write-Host -ForegroundColor Green $X500 `n
-        Write-Host -ForegroundColor DarkCyan "Here is the Set-Mailbox command to add the X.500 address to a user (change the Identity attribute accordingly): `n"
-        Write-Host -ForegroundColor Green "Set-Mailbox -Identity first.last@domain.com -EmailAddresses @{add=`"$X500`"}" `n
-        Write-Host -ForegroundColor Yellow "Done!`n"
+        # Write-Host -ForegroundColor DarkCyan "Here is the Set-Mailbox command to add the X.500 address to a user (change the Identity attribute accordingly): `n"
+        # Write-Host -ForegroundColor Green "Set-Mailbox -Identity first.last@domain.com -EmailAddresses @{add=`"$X500`"}" `n
+        # Write-Host -ForegroundColor Yellow "Done!`n"
     }
 }
