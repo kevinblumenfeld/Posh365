e9927cc60571c88b7f28f571d4c37de4337f06d5
Kevin Blumenfeld <github@thenext.net>
2019-12-26 20:13:36 -0500

    test authurl

--- Posh365/Private/Security/Get-O365FederationHostname.ps1
+++ Posh365/Private/Security/Get-O365FederationHostname.ps1
@@ -10,6 +10,7 @@ function Get-O365FederationHostname {
     else {
         # Determine the auth URL hostname component. Not as elegant as a regex, but it works
         $authUrlHost = $DomainData.FEDERATION.AuthURL
+        Write-Host "AUTHURL:`t$authUrlHost"
         $authUrlHost = $authUrlHost.Replace('https://', '') # Remove HTTPS:// from the URL
         $authUrlHost = $authUrlHost.Replace('http://', '') # Remove HTTP:// from the URL, almost 0% chance of this ever existing
         $authUrlHost = $authUrlHost.Split('/')[0] # Split the auth URL, and grab the first component, the hostname
