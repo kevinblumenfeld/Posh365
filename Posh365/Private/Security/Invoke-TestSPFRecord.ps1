276a9e0ca128ebe39be671a574912619f02625a3
Kevin Blumenfeld <github@thenext.net>
2020-02-12 15:02:12 -0500

    correct kitterman url

--- Posh365/Private/Security/Invoke-TestSPFRecord.ps1
+++ Posh365/Private/Security/Invoke-TestSPFRecord.ps1
@@ -6,6 +6,6 @@ function Invoke-TestSPFRecord {
     )
     $web = Invoke-WebRequest -Uri 'http://www.kitterman.com/spf/validate.html'
     $web.forms[0].fields.domain = $DomainName
-    $result = Invoke-RestMethod 'http://www.kitterman.com/getspf2.py' -Body $web.forms[0].fields
+    $result = Invoke-RestMethod 'http://www.kitterman.com/spf/getspf3.py' -Body $web.forms[0].fields
     $result.replace("`r`n", "--")
 }
