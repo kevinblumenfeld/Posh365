9d570348ebecc812cfe08074dc4d0442a5c416c0
Kevin Blumenfeld <github@thenext.net>
2020-07-01 19:02:43 -0400

    working with date

--- Posh365/Public/Graph/Get-GraphMailFolderMessage.ps1
+++ Posh365/Public/Graph/Get-GraphMailFolderMessage.ps1
@@ -11,27 +11,21 @@ function Get-GraphMailFolderMessage {
         $WellKnownFolder,
 
         [Parameter()]
-        [Int]
-        $Days,
+        [datetime]
+        $Date,
 
         [Parameter(ValueFromPipeline)]
         $MailboxList
 
     )
     begin {
-        $Date = (Get-Date).AddDays(- $Days)
+        Connect-PoshGraph -Tenant $Tenant
     }
     process {
         foreach ($Mailbox in $MailboxList) {
-            Connect-PoshGraph -Tenant $Tenant
-            #$filter = '/?`$filter=ReceivedDateTime ge {0}' -f $Date.ToUniversalTime().ToString("O")
-            $filter = "/?`$filter=ReceivedDateTime le 2018-12-26T19:14:29Z"
-
-            # $filter = '/?`$select=ReceivedDateTime,Sender,Subject,IsRead,InferenceClassification`&`$Top=1000`&`$filter=ReceivedDateTime ge {0}' -f $Date.ToUniversalTime().ToString("O")
+            $filter = "/?`$filter=ReceivedDateTime le {0}" -f $Date.ToUniversalTime().ToString("O")
             $RestSplat = @{
-                #Uri     = "https://graph.microsoft.com/beta/users/{0}/mailFolders('{1}')/messages" -f $Mailbox.UserPrincipalName, $WellKnownFolder
                 Uri     = "https://graph.microsoft.com/beta/users/{0}/mailFolders('{1}')/messages{2}" -f $Mailbox.UserPrincipalName, $WellKnownFolder, $filter
-                # Uri     = "https://graph.microsoft.com/beta/users/'test100@kevdev.onmicrosoft.com'/mailFolders('Inbox')/messages/?&`$filter=$filter" #-f (Get-Date).AddYears(-1).ToUniversalTime().ToString('O') #-f $Mailbox.UserPrincipalName, $WellKnownFolder #, $Date.ToUniversalTime().ToString("O")
                 Headers = @{ "Authorization" = "Bearer $Token" }
                 Method  = 'Get'
             }
