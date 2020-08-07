bd3a732b5aedca447ffc635ef6dbfed6dc68e77d
Kevin Blumenfeld <github@thenext.net>
2020-08-07 17:24:48 -0400

    Cleaned up Export-AzureADAppandper

--- Posh365/Public/AzureAD/Export-AzureADAppAndPermissions.ps1
+++ Posh365/Public/AzureAD/Export-AzureADAppAndPermissions.ps1
@@ -5,65 +5,71 @@ function Export-AzureADAppAndPermissions {
 
     .DESCRIPTION
     Export Azure AD App name & API permissions to an xml
+    Can be used to create an app with the same permissions and settings
 
     .PARAMETER Name
-    Azure AD Application you wish to export
+    Name of the App to export
+    use either Name or ObjectId, not both
 
-    .PARAMETER ServicePrincipalName
-    Microsoft Service Principals. Tested with the Service Principal 'Microsoft Graph'
+    .PARAMETER ObjectId
+    ObjectId of the app to export
+    use either Name or ObjectId, not both
+
+    .PARAMETER Path
+    Path where the script will save the XML of the exported App
 
     .EXAMPLE
-    Export-GraphAPIPermissions -Name 'TestApp' -ServicePrincipalName 'Microsoft Graph'
+    Export-AzureADAppAndPermissions -ObjectId f230aa0b-9832-431c-a879-a40e57b71d79 -Path C:\temp\apps
+
+    .EXAMPLE
+    Export-AzureADAppAndPermissions -Name TestApp -Path C:\temp\apps
 
     .NOTES
     Output from this function will look like this:
 
-    AzureAD App and API Permissions for TestApp, exported to:
-    C:\Users\kevin.blumenfeld\Desktop\Posh365\GraphApps\kevdev\TestApp-20200712-0757.xml
+    AzureAD App and API Permissions for App Test12345, exported to: C:\Scripts\Test12345-20200807-0519.xml
 
     #>
-
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName = 'Name')]
     param (
-        [Parameter(Mandatory)]
-        $Name
+        [Parameter(Mandatory, ParameterSetName = 'Name')]
+        $Name,
+
+        [Parameter(Mandatory, ParameterSetName = 'ObjectId')]
+        $ObjectId,
+
+        [Parameter(Mandatory, ParameterSetName = 'Name')]
+        [Parameter(Mandatory, ParameterSetName = 'ObjectId')]
+        [ValidateScript( { Test-Path $_ } )]
+        $Path
     )
 
-    $Tenant = Get-AzureADTenantDetail
-    $PoshPath = Join-Path ([Environment]::GetFolderPath("Desktop")) -ChildPath 'Posh365'
-    $GraphPath = Join-Path $PoshPath -ChildPath 'GraphApps'
-    $TenantPath = Join-Path $GraphPath -ChildPath $Tenant.DisplayName
-
-    if (-not (Test-Path $TenantPath)) {
-        $null = New-Item -ItemType Directory -Path $PoshPath -ErrorAction SilentlyContinue
-        $null = New-Item -ItemType Directory -Path $GraphPath -ErrorAction SilentlyContinue
-        $null = New-Item -ItemType Directory -Path $TenantPath -ErrorAction SilentlyContinue
+    if ($PSCmdlet.ParameterSetName -eq 'Name') {
+        $SourceApp = Get-AzureADApplication -filter "DisplayName eq '$Name'"
     }
-
-    $SourceApp = Get-AzureADApplication -filter "DisplayName eq '$Name'"
-    # $ServicePrincipal = Get-AzureADServicePrincipal -filter "DisplayName eq '$ServicePrincipalName'"
+    elseif ($PSCmdlet.ParameterSetName -eq 'ObjectId') {
+        $SourceApp = Get-AzureADApplication -ObjectID $ObjectId
+    }
 
     if (-not $SourceApp) {
         Write-Host "Azure AD Application Name: $Name was not found " -ForegroundColor Red
         continue
     }
-    while (@($SourceApp).count -ne 1 -and @($AppChoice).count -ne 1) {
-
-        $AppObject = foreach ($App in $SourceApp) {
-
-            if ($SecretDate = ($App | Get-AzureADApplicationPasswordCredential).startdate) { $OldestSecret = ($SecretDate | Sort-Object )[0] }
-            else { $OldestSecret = 'No Client Secret' }
-            [PSCustomObject]@{
-                Name         = $App.DisplayName
-                ObjectId     = $App.objectid
-                AppId        = $App.AppId
-                OldestSecret = $OldestSecret
-            }
-        }
-        $AppChoice = $AppObject | Out-GridView -OutputMode Single -Title "Duplicate named apps - Please choose which app to export."
+    if (@($SourceApp).count -gt 1) {
+        $SourceApp | Format-Table -AutoSize
+        Write-Host "Duplicate named apps - Provide re-run with -ObjectID." -ForegroundColor Red
+        continue
     }
-    if ($AppChoice) { $SourceApp = Get-AzureADApplication -filter "DisplayName eq '$AppChoice.Name'" }
 
+    if ($SecretDate = ($SourceApp | Get-AzureADApplicationPasswordCredential).startdate) { $OldestSecret = ($SecretDate | Sort-Object )[0] }
+    else { $OldestSecret = 'No Client Secret' }
+
+    [PSCustomObject]@{
+        Name             = $SourceApp.DisplayName
+        ObjectId         = $SourceApp.objectid
+        AppId            = $SourceApp.AppId
+        OldestSecretDate = $OldestSecret
+    }
     $App = @{
         DisplayName = $SourceApp.DisplayName
         SourceApp   = $SourceApp
@@ -84,8 +90,8 @@ function Export-AzureADAppAndPermissions {
         $App['API'][$Access.ResourceAppId]['ResourceList'] = $RLObj
     }
 
-    $XMLPath = (Join-Path -Path $TenantPath -ChildPath ('{0}-{1}.xml' -f $Name, [DateTime]::Now.ToString('yyyyMMdd-hhmm')) )
+    $XMLPath = (Join-Path -Path $Path -ChildPath ('{0}-{1}.xml' -f $SourceApp.DisplayName, [DateTime]::Now.ToString('yyyyMMdd-hhmm')) )
     $App | Export-Clixml $XMLPath -Force
-    Write-Host "AzureAD App and API Permissions for TestApp $Name, exported to:" -ForegroundColor Cyan
-    $XMLPath
+    Write-Host "`r`nAzureAD App and API Permissions for App $($SourceApp.DisplayName), exported to: " -ForegroundColor Cyan -NoNewline
+    Write-Host "$XMLPath`r`n" -ForegroundColor Yellow
 }
