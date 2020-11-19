d32c4ec1e037386196b774dc39dd136f8b542786
Kevin Blumenfeld <github@thenext.net>
2020-11-19 11:43:45 -0500

    Android Protection Policies first go

--- Posh365/Public/Intune/Get-MemMobileAppProtectionAndroid.ps1
+++ Posh365/Public/Intune/Get-MemMobileAppProtectionAndroid.ps1
@@ -7,6 +7,14 @@ function Get-MemMobileAppProtectionAndroid {
         'deploymentSummary', 'exemptedAppPackages', 'displayName', 'allowedAndroidDeviceModels'
         'approvedKeyboards', 'deploymentSummary@odata.context', 'apps@odata.context'
         'deploymentSummary@odata.context', 'assignments@odata.context'
+        'allowedInboundDataTransferSources', 'allowedOutboundDataTransferDestinations'
+        'allowedOutboundClipboardSharingLevel', 'allowedOutboundClipboardSharingExceptionLength'
+        'managedBrowserToOpenLinksRequired', 'managedBrowser', 'createdDateTime'
+        'lastModifiedDateTime', 'roleScopeTagIds', 'version', 'id'
+        'pinRequired', 'maximumPinRetries', 'simplePinBlocked', 'minimumPinLength'
+        'pinCharacterSet', 'periodBeforePinReset', 'disableAppPinIfDevicePinIsSet'
+        'appActionIfMaximumPinRetriesExceeded', 'pinRequiredInsteadOfBiometricTimeout'
+        'previousPinBlockCount'
     )
     Get-MemMobileAppProtectionAndroidData | Select-Object -ExcludeProperty $Excludes -Property @(
         @{
@@ -29,6 +37,66 @@ function Get-MemMobileAppProtectionAndroid {
             Name       = 'allowedDataStorageLocations'
             Expression = { @($_.allowedDataStorageLocations) -ne '' -join "`r`n" }
         }
+        @{
+            Name       = 'allowedInboundDataTransferSources'
+            Expression = { $_.allowedInboundDataTransferSources }
+        }
+        @{
+            Name       = 'allowedOutboundDataTransferDestinations'
+            Expression = { $_.allowedOutboundDataTransferDestinations }
+        }
+        @{
+            Name       = 'allowedOutboundClipboardSharingLevel'
+            Expression = { $_.allowedOutboundClipboardSharingLevel }
+        }
+        @{
+            Name       = 'allowedOutboundClipboardSharingExceptionLength'
+            Expression = { $_.allowedOutboundClipboardSharingExceptionLength }
+        }
+        @{
+            Name       = 'managedBrowserToOpenLinksRequired'
+            Expression = { $_.managedBrowserToOpenLinksRequired }
+        }
+        @{
+            Name       = 'pinRequired'
+            Expression = { $_.pinRequired }
+        }
+        @{
+            Name       = 'maximumPinRetries'
+            Expression = { $_.maximumPinRetries }
+        }
+        @{
+            Name       = 'simplePinBlocked'
+            Expression = { $_.simplePinBlocked }
+        }
+        @{
+            Name       = 'minimumPinLength'
+            Expression = { $_.allowedOutboundClipboardSharingLevel }
+        }
+        @{
+            Name       = 'pinCharacterSet'
+            Expression = { $_.allowedOutboundClipboardSharingExceptionLength }
+        }
+        @{
+            Name       = 'periodBeforePinReset'
+            Expression = { $_.managedBrowserToOpenLinksRequired }
+        }
+        @{
+            Name       = 'disableAppPinIfDevicePinIsSet'
+            Expression = { $_.disableAppPinIfDevicePinIsSet }
+        }
+        @{
+            Name       = 'appActionIfMaximumPinRetriesExceeded'
+            Expression = { $_.appActionIfMaximumPinRetriesExceeded }
+        }
+        @{
+            Name       = 'pinRequiredInsteadOfBiometricTimeout'
+            Expression = { $_.disableAppPinIfDevicePinIsSet }
+        }
+        @{
+            Name       = 'previousPinBlockCount'
+            Expression = { $_.managedBrowser }
+        }
         '*'
         @{
             Name       = 'allowedAndroidDeviceModels'
@@ -41,6 +109,26 @@ function Get-MemMobileAppProtectionAndroid {
         @{
             Name       = 'exemptedAppPackages'
             Expression = { @($_.exemptedAppPackages.foreach{ '{0} --> {1}' -f $_.Name, $_.Value }) -ne '' -join "`r`n" }
+        }
+        @{
+            Name       = 'createdDateTime'
+            Expression = { $_.createdDateTime }
+        }
+        @{
+            Name       = 'lastModifiedDateTime'
+            Expression = { $_.lastModifiedDateTime }
+        }
+        @{
+            Name       = 'roleScopeTagIds'
+            Expression = { $_.roleScopeTagIds }
+        }
+        @{
+            Name       = 'id'
+            Expression = { $_.id }
+        }
+        @{
+            Name       = 'version'
+            Expression = { $_.version }
         }
     )
 }
